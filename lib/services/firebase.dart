36d693772650373ffeec400e6cd83c85fbb357e3
SirRamNick <ramenicholas.tiongson@olopsc.edu.ph>
2024-05-07 11:12:33 +0800

    updated services

--- lib/services/firebase.dart
+++ lib/services/firebase.dart
@@ -3,5 +3,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 //test
 
 class FirestoreService {
-  final CollectionReference = FirebaseFirestore.instance.collection('alumni');
+  final CollectionReference alumni =
+      FirebaseFirestore.instance.collection('alumni');
+
+  Future addAlumnus(
+      String firstName, String lastName, String program, int yearGraduated) {
+    return alumni.add({
+      'first_name': firstName,
+      'last_name': lastName,
+      'program': program,
+      'year_graduated': yearGraduated,
+    });
+  }
+
+  Stream get displayAlumni => alumni.snapshots();
 }
