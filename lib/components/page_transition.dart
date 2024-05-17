ab7ba9eabf356f4be0c1d6a08bc88f398705f8ee
olopsc-rxaliman <raliman22a-0077@olopsc.edu.ph>
2024-05-17 12:03:52 +0800

    Tweaks on app drawer, added attributions

--- lib/components/page_transition.dart
+++ lib/components/page_transition.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 PageRouteBuilder instantTransitionTo(Widget page) => PageRouteBuilder(
-  pageBuilder: (context, animation1, animation2) => page,
-  transitionDuration: Duration.zero,
-  reverseTransitionDuration: Duration.zero,
-);
+      pageBuilder: (context, animation1, animation2) => page,
+      transitionDuration: Duration.zero,
+      reverseTransitionDuration: Duration.zero,
+    );
+
+MaterialPageRoute normalTransitionTo(Widget page) => MaterialPageRoute(
+      builder: (context) => page,
+    );
